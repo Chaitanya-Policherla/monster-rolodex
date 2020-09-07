a99524665e21420dff8598346d5389b05c6c8198
Policherla, Chaitanya Kumar <chaitanya-kumar.policherla@metlife.com>
2020-09-07 21:32:48 +0530

    Updates

---
+++ precache-manifest.b2b5852a3b6ab043248f5785cfc9b636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6628a6e25cab319819871b4b287dd7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5e58846924563e14f1a7",
+    "url": "/monster-rolodex/static/css/main.d3b8dc97.chunk.css"
+  },
+  {
+    "revision": "082f8914de693280636e",
+    "url": "/monster-rolodex/static/js/2.9cbb02df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9cbb02df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e58846924563e14f1a7",
+    "url": "/monster-rolodex/static/js/main.04a97a12.chunk.js"
+  },
+  {
+    "revision": "192894fcf2c1737177cd",
+    "url": "/monster-rolodex/static/js/runtime-main.df1e3023.js"
+  }
+]);
